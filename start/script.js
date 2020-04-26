5f62b334ee92adce94db5f4ca18a1555cd1a0d3f
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-04-25 19:55:13 -0500

    added animation

--- start/script.js
+++ start/script.js
@@ -1,2 +1,22 @@
 $(document).ready(function() {
+
+  // Credit for Toggle Background Color: https://www.geeksread.com/javascript-lesson-36-dom-how-to-toggle-background-color/
+
+  var key = document.querySelector("#key");
+  var backgroundColor = "background-color";
+  var color = "color";
+  var filter = "filer";
+  var isLight = false;
+
+  key.addEventListener("click", function() {
+    if (isLight) {
+      document.body.style.backgroundColor = "black",
+      document.body.style.color= "white";
+      isLight = false;
+    } else {
+      document.body.style.backgroundColor = "white",
+      document.body.style.color= "black";
+      isLight = true;
+    }
+  });
 });
