320d89c1da20ab9ee113c2acdd62c59cd7dd093f
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-04-27 22:54:07 -0500

    GOT THE KEY TO WORK

--- unlock/script.js
+++ unlock/script.js
@@ -2,27 +2,49 @@ console.log("hello world!");
 
 $(document).ready(function() {
 
-    $("#key").hover(function() {
-      $("body").css("background-color", "white"),
-        $("body").css("color", "black");
+var transition = function() {
+
+    $("body").css({
+      "background-color": "white",
+      "color": "black"
     });
 
-    $("#key").hover(function() {
-      $("#key").css("filter", "invert(0)");
+    $("#key").css({
+      "filter": "invert(0)"
     });
 
-    // Credit for jQuery Rotation Animation: https://codepen.io/seeker5084/pen/VMQGwX
-    $('#key').hover(function() {
-      $('#key').animate({
-        deg: 90
-      }, {
-        duration: 1000,
-        step: function(now) {
-          $(this).css({
-            transform: 'rotate(' + now + 'deg)'
-          });
-        }
-      });
+  // Credit for jQuery Rotation Animation: https://codepen.io/seeker5084/pen/VMQGwX
+    $('#key').animate({
+      deg: 90
+    }, {
+      duration: 1000,
+      step: function(now) {
+        $(this).css({
+          transform: 'rotate(' + now + 'deg)'
+        });
+      }
     });
+
+    $("#key").animate({
+      top: "250",
+      opacity: 0
+    }, 1000, function() {
+      $("#key").remove()
+    });
+
+    }
+
+    $("#key").on("click", function() {
+      transition();
+    });
+
+// Credit for transition for animation to complete: https://forum.webflow.com/t/delay-page-transition-for-animation-to-complete/52793
+   $("#key").on("click", function(e) {
+    e.preventDefault();
+    var linkUrl = $("a").attr('href');
+    setTimeout(function(url) {
+      window.location = url;
+    }, 2000, linkUrl);
+  });
 
 });
