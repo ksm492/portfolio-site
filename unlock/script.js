afed01e5644b29b4754b36696b76a5cc5d67c3d2
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-04-27 22:09:16 -0500

    built link to home

--- unlock/script.js
+++ unlock/script.js
@@ -1,38 +1,28 @@
+console.log("hello world!");
+
 $(document).ready(function() {
 
-  $("#key").on("click", function() {
-    $("body").css({
-      "background-color": "white",
-      "color": "black"
+    $("#key").hover(function() {
+      $("body").css("background-color", "white"),
+        $("body").css("color", "black");
     });
-  });
 
-  $("#key").on("click", function() {
-    $("#key").css({
-      "filter": "invert(0)"
+    $("#key").hover(function() {
+      $("#key").css("filter", "invert(0)");
     });
-  });
 
-  // Credit for jQuery Rotation Animation: https://codepen.io/seeker5084/pen/VMQGwX
-  $('#key').on("click", function() {
-    $('#key').animate({
-      deg: 90
-    }, {
-      duration: 1000,
-      step: function(now) {
-        $(this).css({
-          transform: 'rotate(' + now + 'deg)'
-        });
-      }
+    // Credit for jQuery Rotation Animation: https://codepen.io/seeker5084/pen/VMQGwX
+    $('#key').hover(function() {
+      $('#key').animate({
+        deg: 90
+      }, {
+        duration: 1000,
+        step: function(now) {
+          $(this).css({
+            transform: 'rotate(' + now + 'deg)'
+          });
+        }
+      });
     });
-  });
 
-  $("#key").on("click", function() {
-    $("#key").animate({
-      top: "250",
-      opacity: 0
-    }, 1000, function() {
-      $("#key").remove()
-    });
-  });
 });
