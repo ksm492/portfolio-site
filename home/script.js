cbfdd489012fa17a53bfc461af966c6b76334b79
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-05-03 17:09:31 -0500

    working on home

--- home/script.js
+++ home/script.js
@@ -2,4 +2,50 @@ console.log("i'm here!");
 
 $(document).ready(function() {
 
+  $("#projects").mouseenter(function() {
+    $("body").css({"background-color": "cyan"})
+  });
+
+  $("#projects").mouseleave(function() {
+    $("body").css({"background-color": "white"})
+  });
+
+  $("#projects").hover(function() {
+    $("#assignments").toggle(),
+    $("#try-outs").toggle(),
+    $("#about").toggle(),
+    $("#contact").toggle()
+  });
+
+  $("#assignments").mouseenter(function() {
+    $("body").css({"background-color": "magenta"})
+  });
+
+  $("#assignments").mouseleave(function() {
+    $("body").css({"background-color": "white"})
+  });
+
+  $("#assignments").hover(function() {
+    $("#projects").toggle(),
+    $("#try-outs").toggle(),
+    $("#about").toggle(),
+    $("#contact").toggle()
+  });
+
+  $("#try-outs").mouseenter(function() {
+    $("body").css({"background-color": "yellow"})
+  });
+
+  $("#try-outs").mouseleave(function() {
+    $("body").css({"background-color": "white"})
+  });
+
+  $("#try-outs").hover(function() {
+    $("#projects").toggle(),
+    $("#assignments").toggle(),
+    $("#about").toggle(),
+    $("#contact").toggle()
+  });
+
+
 });
